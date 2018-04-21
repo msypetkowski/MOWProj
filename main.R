3080dcc2529e914b46ee6cb478475c3696ac388f
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2018-04-21 14:21:16 +0200

    Add clustering

--- main.R
+++ main.R
@@ -1,5 +1,8 @@
 # Simple experiment
 require(randomForest)
+require(ggplot2)
+require(cluster)
+require(ade4)
 
 getData <- function(id) {
 	d1=read.table("student-mat.csv",sep=",",header=TRUE)
@@ -12,25 +15,45 @@ getData <- function(id) {
 
 	dataset
 
-	## use code below for experimenting with merged tables
-	## keep in mind that G1, G2, G3 are different in both tables
-	# dataset <- rbind(d1, d2)
-	# remove duplicate rows (basing on some collumns)
-	# oldNrow = nrow(dataset)
-	# duplicateCriteria = c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
-	# dataset <- dataset[!duplicated(dataset[duplicateCriteria]),]
-	# nrow(dataset)
-	# stopifnot(oldNrow - nrow(dataset) == 382) # as written in description on kaggle
+}
+
+merged_data <- function(d1, d2) {
+    # G1, G2, G3 are different in both tables
+    dataset <- rbind(d1, d2)
+    # remove duplicate rows (basing on some collumns)
+    oldNrow = nrow(dataset)
+    duplicateCriteria = c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+    dataset <- dataset[!duplicated(dataset[duplicateCriteria]),]
+    nrow(dataset)
+    stopifnot(oldNrow - nrow(dataset) == 382) # as written in description on kaggle
+    dataset
 }
 
 # convert 2 ordinal attributes (Walc, Dalc) to 1 binary (alc)
 walc_dalc_to_alc <- function(dataset) {
     # visualize 2 attributes
-    library(ggplot2)
-    qplot(dataset$Walc, dataset$Dalc, geom='bin2d')
     # TODO (optional) : find better visualization
+    print(qplot(dataset$Walc, dataset$Dalc, geom='bin2d'))
 
-    # TODO
+    # do clustering
+    df <- data.frame(dataset$Walc, dataset$Dalc)
+    plot(df)
+    # class 1 will mean non-drinking, 2 - drinking
+    km <- kmeans(df, centers=rbind(c(3,3), c(1,1)))
+    kmeansRes<-factor(km$cluster)
+    s.class(df,fac=kmeansRes, add.plot=TRUE, col=rainbow(nlevels(kmeansRes)))
+
+    # do clustering - increased Dalc importance
+    df <- data.frame(dataset$Walc, dataset$Dalc * 1.5)
+    plot(df)
+    km <- kmeans(df, centers=rbind(c(3,3), c(1,1)))
+    kmeansRes<-factor(km$cluster)
+    s.class(df,fac=kmeansRes, add.plot=TRUE, col=rainbow(nlevels(kmeansRes)))
+
+    # delete Walc and Dalc columnts and append new - Drink
+    ret <- dataset[ , -which(names(dataset) %in% c("Walc","Dalc"))]
+    ret$Drink <- km$cluster
+    ret
 }
 
 doExperiment <- function(dataset) {
@@ -55,6 +78,6 @@ doExperiment <- function(dataset) {
 	mean(abs(predicted - expected))
 }
 
+dataset <- walc_dalc_to_alc(merged_data(getData(1), getData(2)))
 # doExperiment(getData(1))
 # doExperiment(getData(2))
-walc_dalc_to_alc(getData(1))
