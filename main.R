34ad2677fea57762975f3f68a487f50f229b4791
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2018-04-21 20:35:09 +0200

    Fix random forest experiment, convert tabs to spaces

--- main.R
+++ main.R
@@ -9,15 +9,15 @@ library(rattle)
 library(rpart.plot)
 
 getData <- function(id) {
-	d1=read.table("student-mat.csv",sep=",",header=TRUE)
-	d2=read.table("student-por.csv",sep=",",header=TRUE)
-	if (id == 1) {
-		dataset <- d1
-	} else {
-		dataset <- d2
-	}
+    d1=read.table("student-mat.csv",sep=",",header=TRUE)
+    d2=read.table("student-por.csv",sep=",",header=TRUE)
+    if (id == 1) {
+        dataset <- d1
+    } else {
+        dataset <- d2
+    }
 
-	dataset
+    dataset
 
 }
 
@@ -80,16 +80,17 @@ walc_dalc_to_alc <- function(dataset) {
 
     ret <- rbind(d1, d2)
     ret <- ret[ , -which(names(ret) %in% c("Walc","Dalc"))]
+    ret$Drink <- as.factor(ret$Drink) # we want classification
     stopifnot(nrow(ret) == nrow(dataset))
     stopifnot(ncol(ret) == ncol(dataset) - 1)
     ret
 }
 
 doExperimentNormalTree <- function(dataset) {
-	param <- Drink ~ (school+sex+age+address+famsize+Pstatus+Medu+Fedu+Mjob+Fjob
-					+reason+guardian+traveltime+studytime+failures+schoolsup+famsup
-					+paid+activities+nursery+higher+internet+romantic+famrel+freetime
-					+goout+health+absences+G1+G2+G3)
+    param <- Drink ~ (school+sex+age+address+famsize+Pstatus+Medu+Fedu+Mjob+Fjob
+                    +reason+guardian+traveltime+studytime+failures+schoolsup+famsup
+                    +paid+activities+nursery+higher+internet+romantic+famrel+freetime
+                    +goout+health+absences+G1+G2+G3)
     tr <- rpart(param,data = dataset,method="class",control =rpart.control(minsplit = 30,minbucket=12, cp=0.005,maxdepth=10))
     # prp(tr)
     fancyRpartPlot(tr)
@@ -98,25 +99,26 @@ doExperimentNormalTree <- function(dataset) {
 }
 
 doExperiment <- function(dataset) {
-	# separate a test set from dataset
-	smp_size <- floor(0.75 * nrow(dataset))
-	set.seed(123)
-	# R doesn't have unpack syntax, so a nice oneliner is probably not possible
-	train_ind <- sample(seq_len(nrow(dataset)), size = smp_size)
-	train <- dataset[train_ind, ]
-	test <- dataset[-train_ind, ]
+    # separate a test set from dataset
+    smp_size <- floor(0.75 * nrow(dataset))
+    set.seed(123)
+    # R doesn't have unpack syntax, so a nice oneliner is probably not possible
+    train_ind <- sample(seq_len(nrow(dataset)), size = smp_size)
+    train <- dataset[train_ind, ]
+    test <- dataset[-train_ind, ]
 
-	param <- Drink ~ (school+sex+age+address+famsize+Pstatus+Medu+Fedu+Mjob+Fjob
-					+reason+guardian+traveltime+studytime+failures+schoolsup+famsup
-					+paid+activities+nursery+higher+internet+romantic+famrel+freetime
-					+goout+health+absences+G1+G2+G3)
-	fit <- randomForest(param, data=train, importance=TRUE, ntree=2000)
+    param <- Drink ~ (school+sex+age+address+famsize+Pstatus+Medu+Fedu+Mjob+Fjob
+                    +reason+guardian+traveltime+studytime+failures+schoolsup+famsup
+                    +paid+activities+nursery+higher+internet+romantic+famrel+freetime
+                    +goout+health+absences+G1+G2+G3)
+    fit <- randomForest(param, data=train, importance=TRUE, ntree=2000)
 
-	# results
-	varImpPlot(fit)
-	predicted = predict(fit, test)
-	expected = test$Drink
-	mean(abs(predicted - expected))
+    # results
+    varImpPlot(fit)
+    predicted = predict(fit, test)
+    print(predicted)
+    expected = test$Drink
+    sum(predicted!= expected) / nrow(test)
 }
 
 # only draws plots
@@ -126,5 +128,8 @@ dataset <- walc_dalc_to_alc(getData(1))
 doExperimentNormalTree(dataset)
 dataset <- walc_dalc_to_alc(getData(2))
 doExperimentNormalTree(dataset)
-# doExperiment(getData(1))
-# doExperiment(getData(2))
+
+dataset <- walc_dalc_to_alc(getData(1))
+print(doExperiment(dataset))
+dataset <- walc_dalc_to_alc(getData(2))
+print(doExperiment(dataset))
