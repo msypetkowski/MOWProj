5cff08d040f6ebb0f6a3056ca69beb03e71ceddb
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2018-04-21 20:05:55 +0200

    Add single decision tree experiment

--- main.R
+++ main.R
@@ -4,6 +4,10 @@ require(ggplot2)
 require(cluster)
 require(ade4)
 
+library(rpart)
+library(rattle)
+library(rpart.plot)
+
 getData <- function(id) {
 	d1=read.table("student-mat.csv",sep=",",header=TRUE)
 	d2=read.table("student-por.csv",sep=",",header=TRUE)
@@ -29,8 +33,8 @@ merged_data <- function(d1, d2) {
     dataset
 }
 
-# convert 2 ordinal attributes (Walc, Dalc) to 1 binary (alc)
-walc_dalc_to_alc <- function(dataset) {
+# find criteruim for converting 2 ordinal attributes (Walc, Dalc) to 1 binary (alc)
+calc_clustering <- function(dataset) {
     # visualize 2 attributes
     # TODO (optional) : find better visualization
     print(qplot(dataset$Walc, dataset$Dalc, geom='bin2d'))
@@ -39,21 +43,58 @@ walc_dalc_to_alc <- function(dataset) {
     df <- data.frame(dataset$Walc, dataset$Dalc)
     plot(df)
     # class 1 will mean non-drinking, 2 - drinking
-    km <- kmeans(df, centers=rbind(c(3,3), c(1,1)))
+    km <- kmeans(df, centers=rbind(c(1,1), c(3,3)))
     kmeansRes<-factor(km$cluster)
     s.class(df,fac=kmeansRes, add.plot=TRUE, col=rainbow(nlevels(kmeansRes)))
 
     # do clustering - increased Dalc importance
     df <- data.frame(dataset$Walc, dataset$Dalc * 1.5)
     plot(df)
-    km <- kmeans(df, centers=rbind(c(3,3), c(1,1)))
+    km <- kmeans(df, centers=rbind(c(1,1), c(3,3)))
     kmeansRes<-factor(km$cluster)
     s.class(df,fac=kmeansRes, add.plot=TRUE, col=rainbow(nlevels(kmeansRes)))
 
-    # delete Walc and Dalc columnts and append new - Drink
-    ret <- dataset[ , -which(names(dataset) %in% c("Walc","Dalc"))]
-    ret$Drink <- km$cluster
+    # ret <- dataset[ , -which(names(dataset) %in% c("Walc","Dalc"))]
+    # ret$Drink <- km$cluster
+    # ret
+}
+
+# convert 2 ordinal attributes (Walc, Dalc) to 1 binary (alc)
+walc_dalc_to_alc <- function(dataset) {
+    d1 = dplyr::filter(dataset, 
+        (Dalc <= 2 & Walc <= 2) |
+        (Dalc <= 3 & Walc <= 1) |
+        (Dalc <= 1 & Walc <= 3))
+    print(nrow(d1)/nrow(dataset))
+
+    d2 = dplyr::filter(dataset, !(
+        (Dalc <= 2 & Walc <= 2) |
+        (Dalc <= 3 & Walc <= 1) |
+        (Dalc <= 1 & Walc <= 3)))
+    print(nrow(d2)/nrow(dataset))
+
+    stopifnot(nrow(d1) + nrow(d2) == nrow(dataset))
+
+    d1$Drink = 1
+    d2$Drink = 2
+
+    ret <- rbind(d1, d2)
+    ret <- ret[ , -which(names(ret) %in% c("Walc","Dalc"))]
+    stopifnot(nrow(ret) == nrow(dataset))
+    stopifnot(ncol(ret) == ncol(dataset) - 1)
     ret
+}
+
+doExperimentNormalTree <- function(dataset) {
+	param <- Drink ~ (school+sex+age+address+famsize+Pstatus+Medu+Fedu+Mjob+Fjob
+					+reason+guardian+traveltime+studytime+failures+schoolsup+famsup
+					+paid+activities+nursery+higher+internet+romantic+famrel+freetime
+					+goout+health+absences+G1+G2+G3)
+    tr <- rpart(param,data = dataset,method="class",control =rpart.control(minsplit = 30,minbucket=12, cp=0.005,maxdepth=10))
+    # prp(tr)
+    fancyRpartPlot(tr)
+    # plot(tr)
+    # text(tr)
 }
 
 doExperiment <- function(dataset) {
@@ -65,7 +106,7 @@ doExperiment <- function(dataset) {
 	train <- dataset[train_ind, ]
 	test <- dataset[-train_ind, ]
 
-	param <- Dalc ~ (school+sex+age+address+famsize+Pstatus+Medu+Fedu+Mjob+Fjob
+	param <- Drink ~ (school+sex+age+address+famsize+Pstatus+Medu+Fedu+Mjob+Fjob
 					+reason+guardian+traveltime+studytime+failures+schoolsup+famsup
 					+paid+activities+nursery+higher+internet+romantic+famrel+freetime
 					+goout+health+absences+G1+G2+G3)
@@ -74,10 +115,16 @@ doExperiment <- function(dataset) {
 	# results
 	varImpPlot(fit)
 	predicted = predict(fit, test)
-	expected = test$Dalc
+	expected = test$Drink
 	mean(abs(predicted - expected))
 }
 
-dataset <- walc_dalc_to_alc(merged_data(getData(1), getData(2)))
+# only draws plots
+calc_clustering(merged_data(getData(1), getData(2)))
+
+dataset <- walc_dalc_to_alc(getData(1))
+doExperimentNormalTree(dataset)
+dataset <- walc_dalc_to_alc(getData(2))
+doExperimentNormalTree(dataset)
 # doExperiment(getData(1))
 # doExperiment(getData(2))
