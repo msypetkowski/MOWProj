cfd510f90ebd6d092548adeabf7c9dbc7ab3d079
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2018-04-22 21:46:23 +0200

    Implement cross-validation

--- main.R
+++ main.R
@@ -8,6 +8,12 @@ library(rpart)
 library(rattle)
 library(rpart.plot)
 
+joinDataframes <- function(listOfDf) {
+    df <- do.call("rbind", listOfDf)
+    df
+}
+
+# returns dataset (one of 2 possible)
 getData <- function(id) {
     d1=read.table("student-mat.csv",sep=",",header=TRUE)
     d2=read.table("student-por.csv",sep=",",header=TRUE)
@@ -16,12 +22,11 @@ getData <- function(id) {
     } else {
         dataset <- d2
     }
-
     dataset
-
 }
 
-merged_data <- function(d1, d2) {
+# returns merged dataset
+mergedData <- function(d1, d2) {
     # G1, G2, G3 are different in both tables
     dataset <- rbind(d1, d2)
     # remove duplicate rows (basing on some collumns)
@@ -34,7 +39,7 @@ merged_data <- function(d1, d2) {
 }
 
 # find criteruim for converting 2 ordinal attributes (Walc, Dalc) to 1 binary (alc)
-calc_clustering <- function(dataset) {
+calcClustering <- function(dataset) {
     # visualize 2 attributes
     # TODO (optional) : find better visualization
     print(qplot(dataset$Walc, dataset$Dalc, geom='bin2d'))
@@ -60,17 +65,19 @@ calc_clustering <- function(dataset) {
 }
 
 # convert 2 ordinal attributes (Walc, Dalc) to 1 binary (alc)
-walc_dalc_to_alc <- function(dataset) {
+walcToDalc <- function(dataset) {
     d1 = dplyr::filter(dataset, 
         (Dalc <= 2 & Walc <= 2) |
         (Dalc <= 3 & Walc <= 1) |
         (Dalc <= 1 & Walc <= 3))
+    print("Non-drinking studens ratio")
     print(nrow(d1)/nrow(dataset))
 
     d2 = dplyr::filter(dataset, !(
         (Dalc <= 2 & Walc <= 2) |
         (Dalc <= 3 & Walc <= 1) |
         (Dalc <= 1 & Walc <= 3)))
+    print("Drinking studens ratio")
     print(nrow(d2)/nrow(dataset))
 
     stopifnot(nrow(d1) + nrow(d2) == nrow(dataset))
@@ -86,6 +93,18 @@ walc_dalc_to_alc <- function(dataset) {
     ret
 }
 
+# separate a test set from dataset
+randomDivideDataset <- function(dataset, trainRatio) {
+    smp_size <- floor(trainRatio * nrow(dataset))
+    set.seed(123)
+    # R doesn't have unpack syntax, so a nice oneliner is probably not possible
+    train_ind <- sample(seq_len(nrow(dataset)), size = smp_size)
+    train <- dataset[train_ind, ]
+    test <- dataset[-train_ind, ]
+    stopifnot(nrow(train) + nrow(test) == nrow(dataset))
+    list("train" = train, "test" = test)
+}
+
 doExperimentNormalTree <- function(dataset) {
     param <- Drink ~ (school+sex+age+address+famsize+Pstatus+Medu+Fedu+Mjob+Fjob
                     +reason+guardian+traveltime+studytime+failures+schoolsup+famsup
@@ -98,38 +117,66 @@ doExperimentNormalTree <- function(dataset) {
     # text(tr)
 }
 
-doExperiment <- function(dataset) {
-    # separate a test set from dataset
-    smp_size <- floor(0.75 * nrow(dataset))
-    set.seed(123)
-    # R doesn't have unpack syntax, so a nice oneliner is probably not possible
-    train_ind <- sample(seq_len(nrow(dataset)), size = smp_size)
-    train <- dataset[train_ind, ]
-    test <- dataset[-train_ind, ]
+getClassifierError <- function(fit, testset, attrName) {
+    predicted = predict(fit, testset)
+    # print(predicted)
+    sum(predicted!= testset[[attrName]]) / nrow(testset)
+}
 
+trainRandomForestClssifier <- function(trainset) {
     param <- Drink ~ (school+sex+age+address+famsize+Pstatus+Medu+Fedu+Mjob+Fjob
                     +reason+guardian+traveltime+studytime+failures+schoolsup+famsup
                     +paid+activities+nursery+higher+internet+romantic+famrel+freetime
                     +goout+health+absences+G1+G2+G3)
-    fit <- randomForest(param, data=train, importance=TRUE, ntree=2000)
-
-    # results
-    varImpPlot(fit)
-    predicted = predict(fit, test)
-    print(predicted)
-    expected = test$Drink
-    sum(predicted!= expected) / nrow(test)
+    fit <- randomForest(param, data=trainset, importance=TRUE, ntree=200)
+    # varImpPlot(fit)
+    fit
 }
 
-# only draws plots
-calc_clustering(merged_data(getData(1), getData(2)))
+crossValidation <- function(dataset, trainFunction, errorFunction, partitionsCount) {
+    set.seed(123)
+    n <- partitionsCount
+    nr <- nrow(dataset)
+    # TODO: last may be too small
+    f <- rep(1:ceiling(n), each=ceiling(nr/n), length.out=nr)
+    parts <- split(dataset, f)
+    length(parts)
+    errSum <- 0.0
+    for (i in 1:n) {
+        train <- joinDataframes(parts[-i])
+        test <- parts[[i]]
+        stopifnot(nrow(train) + nrow(test) == nrow(dataset))
+        fit <- trainFunction(train)
+        errSum <- errSum + errorFunction(fit, test)
+    }
+    errSum / n
+}
 
-dataset <- walc_dalc_to_alc(getData(1))
-doExperimentNormalTree(dataset)
-dataset <- walc_dalc_to_alc(getData(2))
-doExperimentNormalTree(dataset)
+doExperiment <- function(dataset) {
+    errFun <- function(fit, testset) {
+        getClassifierError(fit, testset, "Drink")
+    }
+    crossValidation(dataset, trainRandomForestClssifier, errFun, 8)
+}
 
-dataset <- walc_dalc_to_alc(getData(1))
-print(doExperiment(dataset))
-dataset <- walc_dalc_to_alc(getData(2))
-print(doExperiment(dataset))
+main <- function() {
+    # only draws plots
+    calcClustering(mergedData(getData(1), getData(2)))
+
+    dataset <- walcToDalc(getData(1))
+    doExperimentNormalTree(dataset)
+    dataset <- walcToDalc(getData(2))
+    doExperimentNormalTree(dataset)
+
+    print("---------------Math")
+    dataset <- walcToDalc(getData(1))
+    print("randomForest error:")
+    print(doExperiment(dataset))
+
+    print("---------------Portugese")
+    dataset <- walcToDalc(getData(2))
+    print("randomForest error:")
+    print(doExperiment(dataset))
+}
+
+main()
